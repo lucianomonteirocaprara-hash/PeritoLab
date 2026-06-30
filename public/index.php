f0da0a6df10084e9904c2b7fbf820b4e617c4755
Luciano Monteiro Caprara <lucianomonteirocaprara@gmail.com>
2026-06-30 16:09:15 -0300

    Add bootstrap debug output

--- public/index.php
+++ public/index.php
@@ -1,13 +1,25 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+echo "INDEX OK<br>";
+flush();
+
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 define('VIEWS_PATH', APP_PATH . '/Views');
 define('TMP_PATH', BASE_PATH . '/tmp');
 
 require_once BASE_PATH . '/core/Router.php';
+echo "Router OK<br>";
+flush();
+
 require_once BASE_PATH . '/core/Controller.php';
+echo "Controller OK<br>";
+flush();
 
 spl_autoload_register(function (string $class): void {
     $file = APP_PATH . '/Controllers/' . $class . '.php';
@@ -16,7 +28,13 @@ spl_autoload_register(function (string $class): void {
     }
 });
 
+echo "Autoload OK<br>";
+flush();
+
 $router = new Router();
+
+echo "Router criado<br>";
+flush();
 
 $router->get('/',           'HomeController',      'index');
 $router->get('/hash',       'HashController',      'index');
@@ -27,5 +45,8 @@ $router->get('/custody',    'CustodyController',   'index');
 $router->post('/custody',   'CustodyController',   'process');
 $router->get('/metadata',   'MetadataController',  'index');
 $router->post('/metadata',  'MetadataController',  'process');
+
+echo "Dispatch<br>";
+flush();
 
 $router->dispatch();
